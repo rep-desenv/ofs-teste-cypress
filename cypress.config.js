48879316256af6b2d79e3cdccf78bc319266f8d7
Pereira <diego.p.pereira@accenture.com>
2024-01-24 19:57:18 -0300

    automação para suit Login

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+const dotenvPlugin = require('cypress-dotenv');
+require('dotenv').config()
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      config = dotenvPlugin(config)
+      config.env.USUARIO = process.env.USUARIO
+      config.env.SENHA = process.env.SENHA
+      return config
+    },
+    chromeWebSecurity: false
+  },
+});
